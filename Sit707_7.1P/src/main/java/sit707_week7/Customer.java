dcac44d435c9dd011b2c3ecba8eb5a87f9cf8411
mukulksingh98 <109273622+mukulksingh98@users.noreply.github.com>
2024-05-10 00:26:10 +1000

    Commit

---
+++ Sit707_7.1P/src/main/java/sit707_week7/Customer.java
@@ -0,0 +1,12 @@
+package sit707_week7;
+
+public class Customer {
+	
+	private String firstName = "Mukul";
+	
+	private String lastName = "Singh";
+	
+	private String email = "s222296609@deakin.edu.au";
+	
+	
+}
